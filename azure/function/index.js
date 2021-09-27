8b641c8f884619d71a2821f5032604ae70369e84
Logan McAnsh <logan@mcan.sh>
2021-09-27 18:32:23 -0400

    chore: update handler
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- azure/function/index.js
+++ azure/function/index.js
@@ -1,15 +1,47 @@
+function createHTML(title, content) {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>${title}</title>
+      </head>
+      <body>
+        ${content}
+      </body>
+    </html>
+  `;
+}
+
 module.exports = async (context, req) => {
-  context.log("JavaScript HTTP trigger function processed a request.");
-  context.log(req);
-  return {
-    status: 200,
-    body: JSON.stringify(
-      {
-        expected: "http://localhost:4280/",
-        received: req.headers["x-ms-original-url"],
+  let url = req.headers["x-ms-original-url"];
+
+  if (url === "/") {
+    return {
+      status: 200,
+      headers: {
+        "Content-Type": "text/html",
       },
-      null,
-      2
-    ),
+      body: createHTML("Azure Functions", "Hello World!"),
+    };
+  }
+
+  if (url === "/page-2") {
+    return {
+      status: 200,
+      headers: {
+        "Content-Type": "text/html",
+      },
+      body: createHTML("Page 2", "Hello From Page 2!"),
+    };
+  }
+
+  return {
+    status: 404,
+    headers: {
+      "Content-Type": "text/html",
+    },
+    body: createHTML("404", "Page Not Found!"),
   };
 };
