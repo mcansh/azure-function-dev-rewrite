dd62223a796a3bb8941fb9b9fa0219256caa1294
Logan McAnsh <logan@mcan.sh>
2021-09-28 10:54:27 -0400

    chore: update patch
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- azure/function/index.js
+++ azure/function/index.js
@@ -6,6 +6,7 @@ function createHTML(title, content) {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>${title}</title>
+        <link rel="stylesheet" href="/build/_assets/global-I5E7NNPL.css" />
       </head>
       <body>
         ${content}
